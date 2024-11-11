4fc62bd29f85d081dca5d5f25772c7a55beb31a6
Nurkhametov Mike <sasha.rostovskaya@bk.ru>
2024-11-11 22:30:43 +0300

    Update text.

--- app/src/main/java/eu/thedarken/diagnosis/ColorPickerPreference.java
+++ app/src/main/java/eu/thedarken/diagnosis/ColorPickerPreference.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011 Sergey Margaritov
+ * Copyright (C) 2011 Sergey Margaritov (still Android Code Archive)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,7 +30,7 @@ import android.widget.LinearLayout;
 /**
  * A preference type that allows a user to choose a time
  *
- * @author Sergey Margaritov
+ * @author Sergey Margaritov (still Android Code Archive)
  */
 public class ColorPickerPreference
         extends
